c0aedceb4e8fc7dbb847513d441dc43410defddc
Vinicius Tonello Rech <vinitrech@gmail.com>
2023-12-22 13:55:13 -0300

    LengthOfLongestSubstring (not optimal) - O(log(n)) - if changed to used just 1 loop it would be O(n)

---
+++ LeetCodeCSharp/LeetCodeCSharp/Medium/LengthOfLongestSubstring.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCodeCSharp.Medium
+{
+    /// <summary>
+    /// This is the solution for the LengthOfLongestSubstring problem.
+    /// </summary>
+    /// 
+    public static class LengthOfLongestSubstring
+    {
+        /// <summary>
+        /// Given a string s, find the length of the longest substring without repeating characters.<br/><br/>
+        /// Example 1:<br/>
+        /// Input: s = "abcabcbb"<br/>
+        /// Output: 3<br/>
+        /// Explanation: The answer is "abc", with the length of 3.<br/><br/>
+        /// Example 2:<br/>
+        /// Input: s = "bbbbb"<br/>
+        /// Output: 1<br/>
+        /// Explanation: The answer is "b", with the length of 1.<br/><br/>
+        /// Example 3:<br/>
+        /// Input: s = "pwwkew"<br/>
+        /// Output: 3<br/>
+        /// Explanation: The answer is "wke", with the length of 3.<br/><br/>
+        /// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int SolveLengthOfLongestSubstring(string s)
+        {
+            var biggestSubstringLength = 0;
+            var charDictionary = new List<char>();
+
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                for (var j = i; j < s.Length; j++)
+                {
+                    if (charDictionary.Contains(s[j]))
+                    {
+                        break;
+                    }
+
+                    charDictionary.Add(s[j]);
+                    biggestSubstringLength = charDictionary.Count > biggestSubstringLength ? charDictionary.Count : biggestSubstringLength;
+                }
+
+                charDictionary.Clear();
+            }
+
+            return biggestSubstringLength;
+        }
+    }
+}
---
+++ LeetCodeCSharp/SolutionsValidator/Medium/LengthOfLongestSubstringValidator.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+using LeetCodeCSharp.Medium;
+using Xunit;
+
+namespace SolutionsValidator.Medium
+{
+    public class LengthOfLongestSubstringValidator
+    {
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        [InlineData(" ", 1)]
+        [InlineData("au", 2)]
+        [InlineData("dvdf", 3)]
+        public void OnSuccess_Should_Find_Correct_Answer_For_AddTwoNumbers(string s, int expectedOutput)
+        {
+            //Arrange+Act
+            var result = LengthOfLongestSubstring.SolveLengthOfLongestSubstring(s);
+
+            //Assert
+            result.Should().Be(expectedOutput);
+        }
+    }
+}
