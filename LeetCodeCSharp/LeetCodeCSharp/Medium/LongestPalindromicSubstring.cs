66ef34b14d667f77d8cffa505fb821b3ef39c58e
Vinicius Tonello Rech <vinitrech@gmail.com>
2023-12-24 17:08:50 -0300

    starting LongestPalindromicSubstring

---
+++ LeetCodeCSharp/LeetCodeCSharp/Medium/LongestPalindromicSubstring.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeCSharp.Medium
+{
+    /// <summary>
+    /// This is the solution for the Longest Palindromic Substring problem.
+    /// </summary>
+    /// 
+    public static class LongestPalindromicSubstring
+    {
+        /// <summary>
+        /// Given a string s, return the longest palindromic substring in s.<br/><br/>
+        /// Example 1:<br/>
+        /// Input: s = "babad"<br/>
+        /// Output: "bab"<br/>
+        /// Explanation: "aba" is also a valid answer.<br/><br/>
+        /// Example 2:<br/>
+        /// Input: s = "cbbd"<br/>
+        /// Output: "bb"
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string SolveLongestPalindromicSubstring(string s)
+        {
+
+        }
+    }
+}
---
+++ LeetCodeCSharp/SolutionsValidator/Medium/LongestPalindromicSubstringValidator.cs
@@ -0,0 +1,23 @@
+using FluentAssertions;
+using LeetCodeCSharp.Medium;
+using Xunit;
+
+namespace SolutionsValidator.Medium
+{
+    public class LongestPalindromicSubstringValidator
+    {
+        [Theory]
+        //[InlineData("babad", "bab")]
+        //[InlineData("cbbd", "bb")]
+        //[InlineData("a", "a")]
+        [InlineData("kyyrjtdplseovzwjkykrjwhxquwxsfsorjiumvxjhjmgeueafubtonhlerrgsgohfosqssmizcuqryqomsipovhhodpfyudtusjhonlqabhxfahfcjqxyckycstcqwxvicwkjeuboerkmjshfgiglceycmycadpnvoeaurqatesivajoqdilynbcihnidbizwkuaoegmytopzdmvvoewvhebqzskseeubnretjgnmyjwwgcooytfojeuzcuyhsznbcaiqpwcyusyyywqmmvqzvvceylnuwcbxybhqpvjumzomnabrjgcfaabqmiotlfojnyuolostmtacbwmwlqdfkbfikusuqtupdwdrjwqmuudbcvtpieiwteqbeyfyqejglmxofdjksqmzeugwvuniaxdrunyunnqpbnfbgqemvamaxuhjbyzqmhalrprhnindrkbopwbwsjeqrmyqipnqvjqzpjalqyfvaavyhytetllzupxjwozdfpmjhjlrnitnjgapzrakcqahaqetwllaaiadalmxgvpawqpgecojxfvcgxsbrldktufdrogkogbltcezflyctklpqrjymqzyzmtlssnavzcquytcskcnjzzrytsvawkavzboncxlhqfiofuohehaygxidxsofhmhzygklliovnwqbwwiiyarxtoihvjkdrzqsnmhdtdlpckuayhtfyirnhkrhbrwkdymjrjklonyggqnxhfvtkqxoicakzsxmgczpwhpkzcntkcwhkdkxvfnjbvjjoumczjyvdgkfukfuldolqnauvoyhoheoqvpwoisniv", "qahaq")]
+        public void OnSuccess_Should_Find_Correct_Answer_For_LongestPalindromicSubstringValidator(string originalString, string expectedOutput)
+        {
+            //Arrange+Act
+            var result = LongestPalindromicSubstring.SolveLongestPalindromicSubstring(originalString);
+
+            //Assert
+            result.Should().Be(expectedOutput);
+        }
+    }
+}
