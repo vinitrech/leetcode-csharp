4cf152aedd982b127570c2841d2e9779240d9087
Vinicius Tonello Rech <vinitrech@gmail.com>
2023-12-30 14:35:31 -0300

    AddBinary

---
+++ LeetCodeCSharp/LeetCodeCSharp/Easy/AddBinary.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace LeetCodeCSharp.Easy
+{
+    /// <summary>
+    /// This is the solution for the AddBinary problem.
+    /// </summary>
+    /// 
+
+    public static class AddBinary
+    {
+        /// <summary>
+        /// Given two binary strings a and b, return their sum as a binary string.<br/><br/>
+        /// Example 1:<br/>
+        ///Input: a = "11", b = "1"<br/>
+        ///Output: "100"<br/><br/>
+        ///Example 2:<br/>
+        ///Input: a = "1010", b = "1011"<br/>
+        ///Output: "10101"<br/><br/>
+        ///Constraints:<br/>
+        ///1 &lt;= a.length, b.length &lt;= 104<br/>
+        ///a and b consist only of '0' or '1' characters.<br/>
+        ///Each string does not contain leading zeros except for the zero itself.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static string SolveAddBinary(string a, string b)
+        {
+            var sum = new StringBuilder();
+            var (i, j, carry) = (a.Length - 1, b.Length - 1, 0);
+
+            while (i >= 0 || j >= 0 || carry > 0)
+            {
+                var (firstDigit, secondDigit) = (i >= 0 ? a[i--] - '0' : 0, j >= 0 ? b[j--] - '0' : 0);
+                var sumDigit = firstDigit + secondDigit + carry;
+
+                carry = sumDigit / 2;
+                sum.Insert(0, sumDigit % 2);
+            }
+
+            return sum.ToString();
+        }
+    }
+}
---
+++ LeetCodeCSharp/SolutionsValidator/Easy/AddBinaryValidator.cs
@@ -0,0 +1,23 @@
+using FluentAssertions;
+using LeetCodeCSharp.Easy;
+using Xunit;
+
+namespace SolutionsValidator.Easy
+{
+    public class AddBinaryValidator
+    {
+        [Theory]
+        [InlineData("11", "1", "100")]
+        [InlineData("1010", "1011", "10101")]
+        [InlineData("0", "0", "0")]
+        [InlineData("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011", "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000")]
+        public void OnSuccess_Should_Find_Correct_Answer_For_AddBinary(string a, string b, string expectedResult)
+        {
+            //Arrange+Act
+            var result = AddBinary.SolveAddBinary(a, b);
+
+            //Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
