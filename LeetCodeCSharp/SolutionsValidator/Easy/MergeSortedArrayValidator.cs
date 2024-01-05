154ec27df9c648a98ed61d0c5e40a1f3963b5d60
Vinicius Tonello Rech <vinitrech@gmail.com>
2024-01-05 16:47:07 -0300

    spacing

--- LeetCodeCSharp/SolutionsValidator/Easy/MergeSortedArrayValidator.cs
+++ LeetCodeCSharp/SolutionsValidator/Easy/MergeSortedArrayValidator.cs
@@ -21,12 +21,12 @@ namespace SolutionsValidator.Easy
         {
             yield return new object[]
             {
-                    new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3, new int[] { 1, 2, 2, 3, 5, 6 }
+                new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3, new int[] { 1, 2, 2, 3, 5, 6 }
             };
 
             yield return new object[]
             {
-                    new int[] { 1 }, 1, Array.Empty<int>(), 0, new int[] { 1 }
+                new int[] { 1 }, 1, Array.Empty<int>(), 0, new int[] { 1 }
             };
 
             yield return new object[]
